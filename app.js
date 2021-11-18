9cb100c6620eadd30a943cea36facaa98c4fddc5
leagman1 <76821252+leagman1@users.noreply.github.com>
2021-11-18 19:24:11 +0100

    yye

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const svManager = require("./lib/svManager.js");
 
 app.set("view engine", "pug");
 
 app.get('/', (req, res) => {
   res.render("index", {title: "What up, dude!", message: "Oh hi, Mark!"});
 })
+
+app.post("/rs", (req, res) => {
+    svManager.test();
+    res.send("ye");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
