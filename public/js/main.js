1efbf110b8dbddf39dfb544a337b178f99a1a1b1
leagman1 <76821252+leagman1@users.noreply.github.com>
2022-06-04 00:09:15 +0200

    Minor updates for 1.0 stability

--- public/js/main.js
+++ public/js/main.js
@@ -2,23 +2,37 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("startServer").addEventListener("click", function(e){
         postData('/sc', {command: "start"})
             .then(data => {
-                console.log(data); // JSON data parsed by `data.json()` call
+                if(data.commandResult){
+                    alert("Started server.\nTo edit settings, you have to stop the server first.");
+                    location.reload();
+                } else {
+                    alert("Server already running.");
+                }
             });
     });
 
     document.getElementById("stopServer").addEventListener("click", function(e){
         postData('/sc', {command: "stop"})
             .then(data => {
-                console.log(data); // JSON data parsed by `data.json()` call
+                if(data.commandResult){
+                    alert("Stopped server.\nYou may edit settings now.");
+                    location.reload();
+                } else {
+                    alert("Server not running.");
+                }
             });
     });
 
     document.getElementById("saveSettings").addEventListener("click", function(e){
         postData('/sc', {command: "save", svSettings: svSettings})
             .then(data => {
-                console.log(data); // JSON data parsed by `data.json()` call
-
-                location.reload();
+                if(data.commandResult){
+                    alert("Saved settings.");
+                    location.reload();
+                } else {
+                    alert("Could not save settings.\nPlease try deez nuts or stepping on a bee.");
+                    location.reload();
+                }
             });
     });
 
